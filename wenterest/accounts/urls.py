243932fd25de94581aafd04cae698dfe44cc5e20
alejux-cr <alejuxcastle@gmail.com>
2019-10-08 23:21:08 -0600

    Fixed the name urls.py within accounts app for knox_views and knox_logout

--- wenterest/accounts/urls.py
+++ wenterest/accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from .api import RegisterUserAPI,LoginAPI,UserAPI
-from knox import views as know_views
+from knox import views as knox_views
 
 urlpatterns = [
     path('api/auth', include('knox.urls')),
     path('api/auth/register', RegisterUserAPI.as_view()),
     path('api/auth/login', LoginAPI.as_view()),
     path('api/auth/user', UserAPI.as_view()),
-    path('api/auth/logout', know_views.LogoutView.as_view(), name='know_logout')
+    path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout')
 ]
